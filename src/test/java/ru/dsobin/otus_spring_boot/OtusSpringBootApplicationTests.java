30709a8564053ef95238dbd36c65a66226d6de1c
Dmitrii Sobin <dsobin@diasoft.ru>
2025-10-02 12:48:08 +0300

    Init commit

---
+++ src/test/java/ru/dsobin/otus_spring_boot/OtusSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dsobin.otus_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
