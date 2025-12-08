cec871f29ca1738eb35d6e589464f3d70b748d5e
Dmitrii Sobin <dsobin@diasoft.ru>
2025-12-08 16:26:58 +0300

    prj/dsobin/lesson6 update message

--- src/main/java/ru/dsobin/otus/spring/boot/shell/BookShellCommands.java
+++ src/main/java/ru/dsobin/otus/spring/boot/shell/BookShellCommands.java
@@ -1,6 +1,8 @@
 package ru.dsobin.otus.spring.boot.shell;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -21,25 +23,41 @@ public class BookShellCommands {
     private final AuthorDao authorDao;
     private final GenreDao genreDao;
     private final ConsoleIOService io;
+    private final MessageSource messageSource;
 
     @ShellMethod("List all books")
     public void listBooks() {
+        var locale = LocaleContextHolder.getLocale();
+        String authorTranslate = messageSource.getMessage("book.Author.translate", null, locale);
+        String genreTranslate = messageSource.getMessage("book.Genre.translate", null, locale);
         bookDao.findAll().forEach(book ->
-                io.print(String.format("%d: %s (Автор: %s, Жанр: %s)%n",
-                        book.getId(), book.getTitle(),
+                io.print(String.format("%d: %s (%s: %s, %s: %s)%n",
+                        book.getId(),
+                        book.getTitle(),
+                        authorTranslate,
                         book.getAuthor().getName(),
+                        genreTranslate,
                         book.getGenre().getName()))
         );
     }
 
     @ShellMethod("Get book by ID")
     public void getBook(@ShellOption long id) {
+        var locale = LocaleContextHolder.getLocale();
         try {
             Book book = bookDao.findById(id);
-            io.print(String.format("Книга: %s (Автор: %s, Жанр: %s)%n",
-                    book.getTitle(), book.getAuthor().getName(), book.getGenre().getName()));
+            String authorTranslate = messageSource.getMessage("book.Author.translate", null, locale);
+            String genreTranslate = messageSource.getMessage("book.Genre.translate", null, locale);
+            String bookTranslate = messageSource.getMessage("book.Book.translate", null, locale);
+            io.print(String.format("%s: %s (%s: %s, %s: %s)%n",
+                    bookTranslate,
+                    book.getTitle(),
+                    authorTranslate,
+                    book.getAuthor().getName(),
+                    genreTranslate,
+                    book.getGenre().getName()));
         } catch (EmptyResultDataAccessException e) {
-            io.print("Книга с ID " + id + " не найдена");
+            io.print(messageSource.getMessage("book.not.found.with.id", new Object[]{id}, locale));
         }
     }
 
@@ -49,11 +67,12 @@ public class BookShellCommands {
             @ShellOption long authorId,
             @ShellOption long genreId) {
 
+        var locale = LocaleContextHolder.getLocale();
         Author author = authorDao.findById(authorId);
         Genre genre = genreDao.findById(genreId);
         Book book = new Book(null, title, author, genre);
         book = bookDao.insert(book);
-        io.print("Создана книга с ID: " + book.getId());
+        io.print(messageSource.getMessage("book.create.with.id", new Object[]{book.getId()}, locale));
     }
 
     @ShellMethod("Update book")
@@ -63,16 +82,18 @@ public class BookShellCommands {
             @ShellOption long authorId,
             @ShellOption long genreId) {
 
+        var locale = LocaleContextHolder.getLocale();
         Author author = authorDao.findById(authorId);
         Genre genre = genreDao.findById(genreId);
         Book book = new Book(id, title, author, genre);
         bookDao.update(book);
-        io.print("Книга обновлена");
+        io.print(messageSource.getMessage("book.update", null, locale));
     }
 
     @ShellMethod("Delete book by ID")
     public void deleteBook(@ShellOption long id) {
         bookDao.deleteById(id);
-        io.print("Книга удалена");
+        var locale = LocaleContextHolder.getLocale();
+        io.print(messageSource.getMessage("book.delete", null, locale));
     }
 }
