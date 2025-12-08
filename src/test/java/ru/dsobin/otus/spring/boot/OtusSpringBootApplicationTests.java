f236902ddb356ab650f5a4470d40fbbfae2bc260
Dmitrii Sobin <dsobin@diasoft.ru>
2025-12-08 14:16:59 +0300

    prj/dsobin/lesson6 init commit

---
+++ src/test/java/ru/dsobin/otus/spring/boot/OtusSpringBootApplicationTests.java
@@ -0,0 +1,20 @@
+package ru.dsobin.otus.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(properties = {
+		InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+		ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
+@ActiveProfiles("test")
+class OtusSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
