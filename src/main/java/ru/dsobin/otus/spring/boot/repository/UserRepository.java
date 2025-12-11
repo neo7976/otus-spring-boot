35720c56fea7998601d45daa1b6a1f98dc9effa3
Dmitrii Sobin <dsobin@diasoft.ru>
2025-12-11 15:52:06 +0300

    prj/dsobin/lesson9 add migration

--- src/main/java/ru/dsobin/otus/spring/boot/repository/UserRepository.java
+++ src/main/java/ru/dsobin/otus/spring/boot/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.dsobin.otus.spring.boot.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.dsobin.otus.spring.boot.model.User;
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"roles"})
     Optional<User> findByUsername(String username);
 }
